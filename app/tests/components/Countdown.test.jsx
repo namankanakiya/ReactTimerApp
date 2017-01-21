bae64214fb3576d24157a3d132823102b5ac3287
Naman Kanakiya <naman.kanakiya@gmail.com>
2017-01-20 22:44:03 -0500

    countdown testing

---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,42 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jquery');
+var TestUtils = require('react-addons-test-utils');
+
+var Countdown = require('Countdown');
+
+describe("Countdown", () => {
+	it("should exist", () => {
+		expect(Countdown).toExist();
+	});
+
+	describe("handleSetCountdown", () => {
+		it("should set state to started and Countdown", (done) => {
+			var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+			countdown.handleSetCountdown(10);
+
+			expect(countdown.state.count).toBe(10);
+			expect(countdown.state.countdownStatus).toBe("started");
+
+			setTimeout(() => {
+				expect(countdown.state.count).toBe(9);
+				done();
+			}, 1001)
+		});
+
+		it("should not go to negative numbers", (done) => {
+			var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+			countdown.handleSetCountdown(1);
+
+			expect(countdown.state.count).toBe(1);
+			expect(countdown.state.countdownStatus).toBe("started");
+
+			setTimeout(() => {
+				expect(countdown.state.count).toBe(0);
+				done();
+			}, 2001)
+		});
+	} );
+
+});
